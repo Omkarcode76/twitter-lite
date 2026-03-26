2f3e48a739f1d312a5e386223f4845a738f39ca1
omkar <omkar076@icloud.com>
2026-03-26 16:53:39 +0530

    feat: add post schema

---
+++ backend/modles/postSchema.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const postSchema = new Schema({
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    user: {
+        type : Schema.Types.ObjectId,
+        ref : User,
+        required : true
+    },
+},
+    {timeStamps : true}
+
+)
+

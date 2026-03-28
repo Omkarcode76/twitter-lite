ebcd2b0d5ab4ec7ded7131ce1a22502622d44ee0
omkar <omkar076@icloud.com>
2026-03-28 12:06:23 +0530

    feat: populate user data in get posts API

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -7,7 +7,7 @@ const postPost = async (req, res) => {
     
     const post = await Post.create({
       content,
-      userId: user.userId,
+      user: user.userId,
     });
 
     res.status(201).json(post);
@@ -21,7 +21,7 @@ const getPost = async (req, res) => {
     try {
         const user = req.user
         
-        const posts = await Post.find({userId: {$ne: user.userid}}).populate("user", "name" "username" "profilepic").sort({createdAt: -1})
+        const posts = await Post.find({user: {$ne: user.userId}}).populate("user",  "name username profilePic").sort({createdAt: -1})
         
         res.status(200).json(posts)
     } catch (error) {

413d3adb253501376b7c1b36543214032ebcd5d7
omkar <omkar076@icloud.com>
2026-03-28 20:50:10 +0530

    feat: add port handeling in backend

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import connectDB from "./connectDB.js"
-import userRoutes from "./routes/userRoutes.js"
-import postRouter from "./routes/postRoutes.js"
-import authUser from "./middleware/userAuthMiddleware.js"
-dotenv.config()
-const app = express()
-const port = 3000
-app.use(express.json())
-app.use(cors())
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./connectDB.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRouter from "./routes/postRoutes.js";
+import authUser from "./middleware/userAuthMiddleware.js";
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
 
-app.use('/user', userRoutes)
-app.use('/post',authUser, postRouter)
+app.use("/user", userRoutes);
+app.use("/post", authUser, postRouter);
 const startServer = async () => {
   try {
-   await connectDB()
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
-})
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
   } catch (error) {
-  console.error("Server failed to start:", error);
-process.exit(1);
+    console.error("Server failed to start:", error);
+    process.exit(1);
   }
-}
+};
 
-startServer()
+startServer();
